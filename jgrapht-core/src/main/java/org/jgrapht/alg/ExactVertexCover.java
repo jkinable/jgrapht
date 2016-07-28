9252c5fb2a0dc988038ec7f82814d8d0323a3813
Joris Kinable <j.kinable@gmail.com>
2016-07-28 12:42:56 -0400

    Removed some system.outs and added NeighborIndex

--- jgrapht-core/src/main/java/org/jgrapht/alg/ExactVertexCover.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/ExactVertexCover.java
@@ -1,21 +1,62 @@
+/* ==========================================
+ * JGraphT : a free Java graph-theory library
+ * ==========================================
+ *
+ * Project Info:  http://jgrapht.sourceforge.net/
+ * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
+ *
+ * (C) Copyright 2003-2008, by Barak Naveh and Contributors.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+/* -----------------
+ * VertexCovers.java
+ * -----------------
+ * (C) Copyright 2003-2008, by Joris Kinable and Contributors.
+ *
+ * Original Author:  Joris Kinable
+ * Contributor(s): Nils Olberg
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ * 28-Jul-2016 : Initial revision (JK);
+ *
+ */
 package org.jgrapht.alg;
 
-import org.jgrapht.Graph;
-import org.jgrapht.Graphs;
 import org.jgrapht.UndirectedGraph;
 import org.jgrapht.VertexFactory;
 import org.jgrapht.alg.util.VertexDegreeComparator;
 import org.jgrapht.generate.RandomGraphGenerator;
 import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleGraph;
 
-import javax.xml.bind.SchemaOutputResolver;
 import java.util.*;
 
 /**
- * Created by jkinable on 7/26/16.
- * TODO: ADD neighbor index
+ * Finds a minimum vertex cover in a undirected graph.
+ * The implementation relies on a recursive algorithm. At each recursive step,
+ * the algorithm picks a unvisited vertex v and distinguishes two cases:
+ * either v has to be added to the vertex cover or all of its neighbors.
+ *
+ * To speed up the implementation, memoization and a bounding procedure are used.
+ * The current implementation can solve instances with 150-250 vertices efficiently.
+ *
+ * TODO JK: add runtime complexity to class description.
+ *
+ * @author Joris Kinable
  */
 public class ExactVertexCover<V,E> extends VertexCovers{
 
@@ -23,6 +64,8 @@ public class ExactVertexCover<V,E> extends VertexCovers{
     private final UndirectedGraph<V,E> graph;
     /** Number of vertices in the graph**/
     private int N;
+    /** Neighbor cache **/
+    NeighborIndex<V,E> neighborIndex;
 
     /** Map for memoization **/
     private Map<BitSet, VertexCover> memo;
@@ -36,7 +79,7 @@ public class ExactVertexCover<V,E> extends VertexCovers{
      * Maximum weight of the vertex cover. In case there is no weight assigned to the vertices, the weight of
      * the cover equals the cover's cardinality.
      */
-    private int boundOnVertexCoverWeight;
+    private int upperBoundOnVertexCoverWeight;
 
     public ExactVertexCover(UndirectedGraph<V,E> graph){
         this.graph=graph;
@@ -46,6 +89,7 @@ public class ExactVertexCover<V,E> extends VertexCovers{
         //Initialize
         memo=new HashMap<>();
         vertices=new ArrayList<>(graph.vertexSet());
+        neighborIndex=new NeighborIndex<>(graph);
         vertexIDDictionary=new HashMap<>();
 
         N=vertices.size();
@@ -54,48 +98,42 @@ public class ExactVertexCover<V,E> extends VertexCovers{
         Collections.sort(vertices, new VertexDegreeComparator<>(graph, false));
         for(int i=0; i<vertices.size(); i++)
             vertexIDDictionary.put(vertices.get(i), i);
-        System.out.println("Vertices: "+vertices);
 
         //Calculate a bound on the maximum depth using heuristics and mathematical bounding procedures.
         //TODO JK: do these 2 heuristics yield different results, does one dominate the other? If so, remove one.
         //TODO JK: lookup mathematical bounding procedures for the min vertex cover problem. The tighter the bound the better.
         //TODO JK: Is there a lower bounding procedure which allows us to prematurely terminate the search once a solution is found which is equal to the lower bound?
-        boundOnVertexCoverWeight =Math.min(
-                            super.find2ApproximationCover(graph).size(),
-                            super.findGreedyCover(graph).size()
+        upperBoundOnVertexCoverWeight =Math.min(
+                            VertexCovers.find2ApproximationCover(graph).size(),
+                            VertexCovers.findGreedyCover(graph).size()
                         );
 
-        System.out.println("Bound; "+boundOnVertexCoverWeight);
         //Invoke recursive algorithm
         VertexCover vertexCover= this.calculateCoverRecursively(0, new BitSet(N), 0);
 
         //Build solution
-        System.out.println("Optimum cover weight: "+vertexCover.weight);
         Set<V> verticesInCover=new LinkedHashSet<>();
         for (int i = vertexCover.cover.nextSetBit(0); i >= 0 && i<N; i = vertexCover.cover.nextSetBit(i+1))
             verticesInCover.add(vertices.get(i));
-        System.out.println("Is cover: "+isCover(verticesInCover, graph));
         return verticesInCover;
 
     }
 
     private VertexCover calculateCoverRecursively(int lastVisited, BitSet visited, int accumulatedWeight){
-//        System.out.println("Solving for: accumulatedWeight: "+accumulatedWeight+" visited: "+printVisited(visited)+" lastVisited: "+vertices.get(lastVisited));
-        //Check memo
+        //Check memoization table
         if(memo.containsKey(visited)) {
-//            System.out.println("Cache hit");
-            return (VertexCover) memo.get(visited).clone();
+            return memo.get(visited).clone();
         }
 
         //Find the next unvisited vertex WITH neighbors (if a vertex has no neighbors, then we don't need to select it
         //because it doesn't cover any edges)
         int indexNextVertex=-1;
-        List<V> neighbors=Collections.emptyList();
+        Set<V> neighbors=Collections.emptySet();
         for(int index=visited.nextClearBit(lastVisited);
             index >=0 && index<N;
             index=visited.nextClearBit(index+1)){
 
-            neighbors=Graphs.neighborListOf(graph,vertices.get(index));
+            neighbors=new LinkedHashSet<>(neighborIndex.neighborsOf(vertices.get(index)));
             for(Iterator<V> it=neighbors.iterator(); it.hasNext(); ) //Exclude all visited vertices
                 if(visited.get(vertexIDDictionary.get(it.next())))
                     it.remove();
@@ -104,34 +142,26 @@ public class ExactVertexCover<V,E> extends VertexCovers{
                 break;
             }
         }
-//        System.out.println("nextVertex: "+(indexNextVertex >= 0 ? vertices.get(indexNextVertex) : indexNextVertex)+" remaining neighbors: "+neighbors);
 
         //Base case 1: all vertices have been visited
         if(indexNextVertex==-1){ //We've visited all vertices, return the base case
             VertexCover vertexCover=new VertexCover(N, 0);
-            if(accumulatedWeight <= boundOnVertexCoverWeight) { //Found new a solution that matches our bound. Tighten the bound.
-//                System.out.println("Found solution with weight: "+accumulatedWeight+" tightening bound to: "+(accumulatedWeight - 1));
-                boundOnVertexCoverWeight = accumulatedWeight - 1;
+            if(accumulatedWeight <= upperBoundOnVertexCoverWeight) { //Found new a solution that matches our bound. Tighten the bound.
+                upperBoundOnVertexCoverWeight = accumulatedWeight - 1;
             }
             return vertexCover;
-        //Pruning: this vertex cover can never be better than the best cover we already have. Return a cover with a high weight, such that the other branch will be preferred over this branch.
-        }else if(accumulatedWeight >= boundOnVertexCoverWeight){
-//            System.out.println("Pruning");
+        //Base case 2 (pruning): this vertex cover can never be better than the best cover we already have. Return a cover with a large weight, such that the other branch will be preferred over this branch.
+        }else if(accumulatedWeight >= upperBoundOnVertexCoverWeight){
             return new VertexCover(N, N);
         }
 
         //Recursion
-        //@TODO JK: Can we use a lower bound or estimation which of these 2 branches produces a better solution? If one of them is likely to produce a better solution,
-        // then that branch should be explored first! Futhermore, if the lower bound+accumulated cost > boundOnVertexCoverWeight, then we may prune.
+        //TODO JK: Can we use a lower bound or estimation which of these 2 branches produces a better solution? If one of them is likely to produce a better solution,
+        //then that branch should be explored first! Futhermore, if the lower bound+accumulated cost > upperBoundOnVertexCoverWeight, then we may prune.
 
-        /* Create 2 branches:
-            Left Branch: vertex v is added to the cover, and we solve for G_{v}
-            Right branch: N(v) are added to the cover, and we solve for G_{N(v) \cup v }.
-            Here N(v) is the set of neighbors of v. G_{v} indicates the graph obtained by removing vertex v and all vertices incident to it.
-         */
+        //Create 2 branches (N(v) denotes the set of neighbors of v. G_{v} indicates the graph obtained by removing vertex v and all vertices incident to it.):
 
-
-        //Right branch:
+        //Right branch (N(v) are added to the cover, and we solve for G_{N(v) \cup v }.):
         BitSet visitedRightBranch= (BitSet) visited.clone();
         visitedRightBranch.set(indexNextVertex);
         for(V v : neighbors)
@@ -141,7 +171,7 @@ public class ExactVertexCover<V,E> extends VertexCovers{
         for(V v : neighbors) //Delayed update of the right cover
             rightCover.addVertex(vertexIDDictionary.get(v), 1);
 
-        //Left branch:
+        //Left branch (vertex v is added to the cover, and we solve for G_{v}):
         BitSet visitedLeftBranch= (BitSet) visited.clone();
         visitedLeftBranch.set(indexNextVertex);
 
@@ -151,37 +181,24 @@ public class ExactVertexCover<V,E> extends VertexCovers{
 
         //Return the best branch
         if(leftCover.weight <= rightCover.weight){
-            memo.put(visited, (VertexCover) leftCover.clone());
+            memo.put(visited, leftCover.clone());
             return leftCover;
         }else{
 
-            memo.put(visited, (VertexCover) rightCover.clone());
+            memo.put(visited, rightCover.clone());
             return rightCover;
         }
 
     }
 
-    public String printVisited(BitSet visited){
-        List<V> visitedVertices=new ArrayList<>();
-        for (int i = visited.nextSetBit(0); i >= 0 && i<N; i = visited.nextSetBit(i+1))
-            visitedVertices.add(vertices.get(i));
-        return visitedVertices.toString();
-    }
-
-
     public class VertexCover{
         public BitSet cover;
         public int weight;
 
-        public VertexCover(int size, int weight){
+        public VertexCover(int size, int initialWeight){
             cover=new BitSet(size);
-            this.weight=weight;
+            this.weight=initialWeight;
         }
-
-//        public VertexCover(int size){
-//            cover=new BitSet(size);
-////            this.weight=weight;
-//        }
 
         //Copy constructor
         public VertexCover(VertexCover vertexCover){
@@ -189,13 +206,8 @@ public class ExactVertexCover<V,E> extends VertexCovers{
             this.weight=vertexCover.weight;
         }
 
-        @Override
-        public Object clone(){
+        public VertexCover clone(){
             return new VertexCover(this);
-        }
-
-        public void addVertex(int vertexIndex){
-            cover.set(vertexIndex);
         }
 
         public void addVertex(int vertexIndex, int weight){
@@ -204,21 +216,10 @@ public class ExactVertexCover<V,E> extends VertexCovers{
         }
     }
 
-    private boolean isCover(
-            Set<V> vertexSet,
-            Graph<V, E> g)
-    {
-        Set<E> uncoveredEdges = new HashSet<>(g.edgeSet());
-
-        for (V v : vertexSet) {
-            uncoveredEdges.removeAll(g.edgesOf(v));
-        }
-
-        return uncoveredEdges.size() == 0;
-    }
-
     //Temporary code, needs to be deleted
     public static void main(String[] args){
+
+//        4-cyle graph (optimal=2):
 //        UndirectedGraph<Integer, DefaultEdge> g1=new SimpleGraph<>(DefaultEdge.class);
 //        Graphs.addAllVertices(g1, Arrays.asList(0,1,2,3));
 //        g1.addEdge(0,1);
@@ -226,9 +227,10 @@ public class ExactVertexCover<V,E> extends VertexCovers{
 //        g1.addEdge(2,3);
 //        g1.addEdge(3,0);
 //        ExactVertexCover<Integer, DefaultEdge> c1=new ExactVertexCover<>(g1);
-//        System.out.println("Cover c1: "+c1.getMinimumVertexCover()); //Optimal: 2
+//        System.out.println("Cover c1: "+c1.getMinimumVertexCover());
 
 
+//        Wheel graph W_8 (Optimal=5)
 //        UndirectedGraph<Integer, DefaultEdge> g2=new SimpleGraph<>(DefaultEdge.class);
 //        Graphs.addAllVertices(g2, Arrays.asList(0,1,2,3,4,5,6,7));
 //        g2.addEdge(1,2);
@@ -246,9 +248,10 @@ public class ExactVertexCover<V,E> extends VertexCovers{
 //        g2.addEdge(0,6);
 //        g2.addEdge(0,7);
 //        ExactVertexCover<Integer, DefaultEdge> c2=new ExactVertexCover<>(g2);
-//        System.out.println("Cover c2: "+c2.getMinimumVertexCover()); //Optimal: 5
+//        System.out.println("Cover c2: "+c2.getMinimumVertexCover());
 
 
+//        Graph where all vertices have degree 3
 //        UndirectedGraph<Integer, DefaultEdge> g3=new SimpleGraph<>(DefaultEdge.class);
 //        Graphs.addAllVertices(g3, Arrays.asList(0,1,2,3,4,5,6,7,8,9,10,11));
 //        g3.addEdge(0,1);
@@ -273,6 +276,7 @@ public class ExactVertexCover<V,E> extends VertexCovers{
 //        System.out.println("Cover c3: "+c3.getMinimumVertexCover()); //Optimal: 7
 //        System.out.println("Cover c3: "+VertexCovers.findMinimumVertexCover(g3)); //Optimal: 7
 
+//        Graph with 6 vertices in the shape >-<
 //        UndirectedGraph<Integer, DefaultEdge> g4=new SimpleGraph<>(DefaultEdge.class);
 //        Graphs.addAllVertices(g4, Arrays.asList(0,1,2,3,4,5));
 //        g4.addEdge(0,2);
